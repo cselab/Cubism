30fbd7f20fc9aeb3fb2656917d0cfa87be10efac
Michalis_Chatzimanolakis <michaich@ethz.ch>
2020-02-15 19:58:28 +0100

    Changed logical reduction

--- include/Cubism/LoadBalancer.h
+++ include/Cubism/LoadBalancer.h
@@ -278,7 +278,10 @@ public:
         }
    
 
-        MPI_Allreduce(MPI_IN_PLACE,&movedBlocks,1,MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD);
+        //MPI_Allreduce(MPI_IN_PLACE,&movedBlocks,1,MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD);
+        int temp = movedBlocks ? 1:0;
+        MPI_Allreduce(MPI_IN_PLACE,&temp,1, MPI_INT, MPI_LOR, MPI_COMM_WORLD);
+        movedBlocks = (temp == 1);
 
 
         {

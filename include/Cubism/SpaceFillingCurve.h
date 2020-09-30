dbf7217754f7b1ddc3fc36cc223846bcc54afa09
Michail Chatzimanolakis <mchatzim@daint105.login.cscs.ch>
2020-09-30 17:36:02 +0200

    Hilbert curves for 2d and 3d mesh

---
+++ include/Cubism/SpaceFillingCurve.h
@@ -0,0 +1,346 @@
+#pragma once
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <stdint.h>
+
+#include<mpi.h>//for debug
+using namespace std;
+
+//#define MortonCurve
+#define HilbertCurve
+
+namespace cubism // AMR_CUBISM
+{
+
+class SpaceFillingCurve
+{
+
+ protected:
+   unsigned int BX, BY, BZ;
+
+   // Copy-pasted from
+   // www.forceflow.be/2013/10/07/morton-encodingdecoding-through-bit-interleaving-implementations
+
+   // method to seperate bits from a given integer 3 positions apart
+   inline uint64_t splitBy3(unsigned int a) const
+   {
+      uint64_t x = a & 0x1fffff; // we only look at the first 21 bits
+      x          = (x | x << 32) &
+          0x1f00000000ffff; // shift left 32 bits, OR with self, and
+                            // 00011111000000000000000000000000000000001111111111111111
+      x = (x | x << 16) &
+          0x1f0000ff0000ff; // shift left 32 bits, OR with self, and
+                            // 00011111000000000000000011111111000000000000000011111111
+      x = (x | x << 8) &
+          0x100f00f00f00f00f; // shift left 32 bits, OR with self, and
+                              // 0001000000001111000000001111000000001111000000001111000000000000
+      x = (x | x << 4) &
+          0x10c30c30c30c30c3; // shift left 32 bits, OR with self, and
+                              // 0001000011000011000011000011000011000011000011000011000100000000
+      x = (x | x << 2) & 0x1249249249249249;
+      return x;
+   }
+
+   inline uint64_t mortonEncode_for(unsigned int x, unsigned int y, unsigned int z) const
+   {
+      uint64_t answer = 0;
+      answer |= splitBy3(x) | splitBy3(y) << 1 | splitBy3(z) << 2;
+      return answer;
+   }
+
+   //// DECODE 3D Morton code : For loop
+   inline void m3D_d_for(int m, int &x, int &y, int &z)
+   {
+      x             = 0;
+      y             = 0;
+      z             = 0;
+      int checkbits = static_cast<int>(floor((sizeof(int) * 8.0f / 3.0f)));
+      for (int i = 0; i <= checkbits; ++i)
+      {
+         int selector       = 1;
+         int shift_selector = 3 * i;
+         int shiftback      = 2 * i;
+         x |= (m & (selector << shift_selector)) >> (shiftback);
+         y |= (m & (selector << (shift_selector + 1))) >> (shiftback + 1);
+         z |= (m & (selector << (shift_selector + 2))) >> (shiftback + 2);
+      }
+   }
+
+   int AxestoTranspose(const unsigned int *X_in, int b) const // position, #bits, dimension
+   {
+      if (b == 0)
+      {
+         assert(X_in[0] == 0);
+         assert(X_in[1] == 0);
+         assert(X_in[2] == 0);
+         return 0;
+      }
+
+      const int n       = 3;
+      unsigned int X[3] = {X_in[0], X_in[1], X_in[2]};
+
+      assert(b - 1 >= 0);
+
+      int M = 1 << (b - 1), P, Q, t;
+      int i;
+
+      // Inverse undo
+      for (Q = M; Q > 1; Q >>= 1)
+      {
+         P = Q - 1;
+         for (i = 0; i < n; i++)
+            if (X[i] & Q) X[0] ^= P; // invert
+            else
+            {
+               t = (X[0] ^ X[i]) & P;
+               X[0] ^= t;
+               X[i] ^= t;
+            }
+      } // exchange
+
+      // Gray encode
+      for (i = 1; i < n; i++) X[i] ^= X[i - 1];
+      t = 0;
+      for (Q = M; Q > 1; Q >>= 1)
+         if (X[n - 1] & Q) t ^= Q - 1;
+      for (i = 0; i < n; i++) X[i] ^= t;
+
+      int retval = 0;
+      int a      = 0;
+      for (int level = 0; level < b; level++)
+      {
+         retval += ((1 << (a)) * (X[2] >> level & 1)) + ((1 << (a + 1)) * (X[1] >> level & 1)) +
+                   ((1 << (a + 2)) * (X[0] >> level & 1));
+         a += 3;
+      }
+
+      return retval;
+   }
+
+   void TransposetoAxes(int index, unsigned int *X, int b) const // position, #bits, dimension
+   {
+      const int n = 3;
+
+      X[0] = 0;
+      X[1] = 0;
+      X[2] = 0;
+
+      int aa = 0;
+      for (int i = 0; index > 0; i++)
+      {
+         int x2 = index % 2;
+         index  = index / 2;
+         int x1 = index % 2;
+         index  = index / 2;
+         int x0 = index % 2;
+         index  = index / 2;
+
+         X[0] += x0 * (1 << aa);
+         X[1] += x1 * (1 << aa);
+         X[2] += x2 * (1 << aa);
+
+         aa += 1;
+      }
+
+      int N = 2 << (b - 1), P, Q, t;
+      int i;
+
+      // Gray decode by H ^ (H/2)
+      t = X[n - 1] >> 1;
+      for (i = n - 1; i >= 1; i--) X[i] ^= X[i - 1];
+      X[0] ^= t;
+
+      // Undo excess work
+      for (Q = 2; Q != N; Q <<= 1)
+      {
+         P = Q - 1;
+         for (i = n - 1; i >= 0; i--)
+            if (X[i] & Q) X[0] ^= P; // invert
+            else
+            {
+               t = (X[0] ^ X[i]) & P;
+               X[0] ^= t;
+               X[i] ^= t;
+            }
+      } // exchange
+   }
+
+ public:
+   int *SUBSTRACT;
+   int base_level;
+   int levelMax;
+
+
+   std::vector< std::vector<int> > i_inverse;
+   std::vector< std::vector<int> > j_inverse;
+   std::vector< std::vector<int> > k_inverse;
+
+   SpaceFillingCurve(){};
+
+   ~SpaceFillingCurve(){ delete[] SUBSTRACT; }
+
+   void __setup(int nx, int ny, int nz)
+   {
+      BX = nx;
+      BY = ny;
+      BZ = nz;
+   }
+
+   SpaceFillingCurve(unsigned int a_BX, unsigned int a_BY, unsigned int a_BZ, int lmax) : BX(a_BX), BY(a_BY), BZ(a_BZ), levelMax(lmax)
+   {
+      std::cout << "Constructing Hilbert curve for " << BX << "x" << BY << "x" << BZ << 
+                   " octree with " << lmax << " levels..." << std::endl;
+      i_inverse.resize(lmax);
+      j_inverse.resize(lmax);
+      k_inverse.resize(lmax);
+      for (int l = 0 ; l < lmax ; l++)
+      {
+        int aux = pow( pow(2,l) , 3);
+        i_inverse[l].resize(BX*BY*BZ*aux,-666);
+        j_inverse[l].resize(BX*BY*BZ*aux,-666);
+        k_inverse[l].resize(BX*BY*BZ*aux,-666);
+      }
+
+      SUBSTRACT = new int[BX * BY * BZ];
+
+      #pragma omp parallel
+      {
+        int n_max  = max(max(BX, BY), BZ);
+        base_level = (log(n_max) / log(2));
+        if (base_level < (double)(log(n_max) / log(2))) base_level++;
+  
+        #pragma omp for collapse(3)
+        for (unsigned int k=0;k<BZ;k++)
+        for (unsigned int j=0;j<BY;j++)
+        for (unsigned int i=0;i<BX;i++)
+        {
+          const unsigned int c[3] = {i,j,k};
+          
+          int index = AxestoTranspose( c, base_level);
+    
+          int substract = 0;
+          for (int h=0; h<index; h++)
+          {
+            unsigned int X[3] = {0,0,0};
+            TransposetoAxes(h, X, base_level);
+            if (X[0] >= BX ||  
+                X[1] >= BY ||  
+                X[2] >= BZ) substract++;   
+          }   
+          SUBSTRACT[(j + k*BY)*BX + i] = substract;
+        }
+      }
+
+      for (int l = 0 ; l < lmax ; l++)
+      {
+        int aux = pow(2,l);
+        #pragma omp parallel for collapse(3)
+        for (unsigned int k=0;k<BZ*aux;k++)
+        for (unsigned int j=0;j<BY*aux;j++)
+        for (unsigned int i=0;i<BX*aux;i++)
+        {
+          int retval = forward(l,i,j,k);
+          i_inverse[l][retval] = i;
+          j_inverse[l][retval] = j;
+          k_inverse[l][retval] = k;
+        }      
+      }
+
+      std::cout << "Hilbert curve ready." << std::endl;
+   }
+
+   // space-filling curve (i,j,k) --> 1D index (given level l)
+   int forward(const unsigned int l, const unsigned int i, const unsigned int j,
+               const unsigned int k) const
+   {
+      unsigned int aux = 1 << l;
+      unsigned int I   = i / aux;
+      unsigned int J   = j / aux;
+      unsigned int K   = k / aux;
+      
+      if (I >= BX || J >= BY || K >= BZ) return 0;
+
+      #if defined(MortonCurve)
+        unsigned int I1 = i % aux;
+        unsigned int J1 = j % aux;
+        unsigned int K1 = k % aux;
+        int z           = mortonEncode_for(I1, J1, K1);
+        int z_origin    = ((J + K * BY) * BX + I) * aux * aux * aux;
+        return z + z_origin;
+      #elif defined(HilbertCurve)
+
+      const unsigned int c2_a[3] = {i, j, k};
+      int s                      = SUBSTRACT[((J + K * BY) * BX + I)] * aux * aux * aux;
+
+      int retval = AxestoTranspose(c2_a, l + base_level) - s;
+
+      return retval;
+#endif
+   }
+
+   void inverse(int Z, unsigned int l, unsigned int &i, unsigned int &j, unsigned int &k)
+   {
+      //unsigned int X[3] = {0, 0, 0};
+      //TransposetoAxes(Z, X, l + base_level);
+      //i = X[0];
+      //j = X[1];
+      //k = X[2];
+
+      i = i_inverse[l][Z];
+      j = j_inverse[l][Z];
+      k = k_inverse[l][Z];
+      assert(i_inverse[l][Z]>=0);
+      assert(j_inverse[l][Z]>=0);
+      assert(k_inverse[l][Z]>=0);
+      //assert(i_inverse[l][Z] < BX * (1<<l));
+      //assert(j_inverse[l][Z] < BY * (1<<l));
+      //assert(k_inverse[l][Z] < BZ * (1<<l));
+   }
+
+   // return 1D index of CHILD of block (i,j,k) at level l (child is at level l+1)
+   int child(int l, int i, int j, int k) { return forward(l + 1, 2 * i, 2 * j, 2 * k); }
+
+   int Encode(int level, int Z, int index[3])
+   {
+      int lmax   = levelMax;
+      int retval = 0;
+
+      int ix = index[0];
+      int iy = index[1];
+      int iz = index[2];
+      for (int l = level; l >= 0; l--)
+      {
+         int Zp = forward(l, ix, iy, iz);
+         retval += Zp;
+         ix /= 2;
+         iy /= 2;
+         iz /= 2;
+      }
+
+      ix = 2 * index[0];
+      iy = 2 * index[1];
+      iz = 2 * index[2];
+      for (int l = level + 1; l < lmax; l++)
+      {
+         int Zc = forward(l, ix, iy, iz);
+
+         Zc -= Zc % 8;
+         retval += Zc;
+
+         unsigned int ix1, iy1, iz1;
+         inverse(Zc, l, ix1, iy1, iz1);
+         ix = 2 * ix1;
+         iy = 2 * iy1;
+         iz = 2 * iz1;
+      }
+
+      retval += level;
+  
+      return retval;
+   }
+};
+
+} // namespace cubism
---
+++ include/Cubism/SpaceFillingCurve2D.h
@@ -0,0 +1,217 @@
+#pragma once
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <stdint.h>
+
+namespace cubism // AMR_CUBISM
+{
+
+class SpaceFillingCurve2D
+{
+
+ protected:
+   unsigned int BX, BY;
+
+
+   //convert (x,y) to d
+   int AxestoTranspose(const unsigned int *X_in, int b) const
+   {
+      unsigned int x = X_in[0];
+      unsigned int y = X_in[1];
+      int n = 1 << b;
+      int rx, ry, s, d=0;
+      for (s=n/2; s>0; s/=2)
+      {
+          rx = (x & s) > 0;
+          ry = (y & s) > 0;
+          d += s * s * ((3 * rx) ^ ry);
+          rot(n, &x, &y, rx, ry);
+      }
+      return d;
+   }
+
+   //convert d to (x,y)
+   void TransposetoAxes(int index, unsigned int *X, int b) const 
+   {
+       // position, #bits, dimension
+       int n = 1 << b;
+       int rx, ry, s, t=index;
+       X[0] = 0;
+       X[1] = 0;
+       for (s=1; s<n; s*=2) {
+           rx = 1 & (t/2);
+           ry = 1 & (t ^ rx);
+           rot(s, &X[0], &X[1], rx, ry);
+           X[0] += s * rx;
+           X[1] += s * ry;
+           t /= 4;
+       }
+   }
+
+   //rotate/flip a quadrant appropriately
+   void rot(int n, unsigned int *x, unsigned int *y, int rx, int ry) const 
+   {
+       if (ry == 0) {
+           if (rx == 1) {
+               *x = n-1 - *x;
+               *y = n-1 - *y;
+           }
+   
+           //Swap x and y
+           int t  = *x;
+           *x = *y;
+           *y = t;
+       }
+   }
+
+
+
+ public:
+   int *SUBSTRACT;
+   int base_level;
+   int levelMax;
+
+
+   std::vector< std::vector<int> > i_inverse;
+   std::vector< std::vector<int> > j_inverse;
+
+   SpaceFillingCurve2D(){};
+
+   ~SpaceFillingCurve2D(){ delete[] SUBSTRACT; }
+
+   void __setup(int nx, int ny)
+   {
+      BX = nx;
+      BY = ny;
+   }
+
+   SpaceFillingCurve2D(unsigned int a_BX, unsigned int a_BY, int lmax) : BX(a_BX), BY(a_BY), levelMax(lmax)
+   {
+      std::cout << "Constructing Hilbert curve for " << BX << "x" << BY <<  " quadree with " << lmax << " levels..." << std::endl;
+      i_inverse.resize(lmax);
+      j_inverse.resize(lmax);
+      for (int l = 0 ; l < lmax ; l++)
+      {
+        int aux = pow( pow(2,l) , 2);
+        i_inverse[l].resize(BX*BY*aux,-666);
+        j_inverse[l].resize(BX*BY*aux,-666);
+      }
+
+      SUBSTRACT = new int[BX * BY];
+
+      #pragma omp parallel
+      {
+        int n_max  = max(BX, BY);
+        base_level = (log(n_max) / log(2));
+        if (base_level < (double)(log(n_max) / log(2))) base_level++;
+  
+        #pragma omp for collapse(2)
+        for (unsigned int j=0;j<BY;j++)
+        for (unsigned int i=0;i<BX;i++)
+        {
+          const unsigned int c[2] = {i,j};
+          
+          int index = AxestoTranspose( c, base_level);
+    
+          int substract = 0;
+          for (int h=0; h<index; h++)
+          {
+            unsigned int X[2] = {0,0};
+            TransposetoAxes(h, X, base_level);
+            if (X[0] >= BX || X[1] >= BY) substract++;   
+          }   
+          SUBSTRACT[j*BX + i] = substract;
+        }
+      }
+
+      for (int l = 0 ; l < lmax ; l++)
+      {
+        int aux = pow(2,l);
+        #pragma omp parallel for collapse(2)
+        for (unsigned int j=0;j<BY*aux;j++)
+        for (unsigned int i=0;i<BX*aux;i++)
+        {
+          int retval = forward(l,i,j);
+          i_inverse[l][retval] = i;
+          j_inverse[l][retval] = j;
+        }      
+      }
+
+      std::cout << "Hilbert curve ready." << std::endl;
+  }
+
+   // space-filling curve (i,j,k) --> 1D index (given level l)
+   int forward(const unsigned int l, const unsigned int i, const unsigned int j) const
+   {
+      unsigned int aux = 1 << l;
+      unsigned int I   = i / aux;
+      unsigned int J   = j / aux;
+      
+      if (I >= BX || J >= BY) return 0;
+
+      const unsigned int c2_a[2] = {i, j};
+      int s                      = SUBSTRACT[J * BX + I]  * aux * aux;
+
+      int retval = AxestoTranspose(c2_a, l + base_level) - s;
+
+      return retval;
+   }
+
+   void inverse(int Z, unsigned int l, unsigned int &i, unsigned int &j)
+   {
+      //unsigned int X[2] = {0, 0};
+      //TransposetoAxes(Z, X, l + base_level);
+      //i = X[0];
+      //j = X[1];
+
+      i = i_inverse[l][Z];
+      j = j_inverse[l][Z];
+      assert(i_inverse[l][Z]>=0);
+      assert(j_inverse[l][Z]>=0);
+      //assert(i_inverse[l][Z] < BX * (1<<l));
+      //assert(j_inverse[l][Z] < BY * (1<<l));
+   }
+
+   // return 1D index of CHILD of block (i,j,k) at level l (child is at level l+1)
+   int child(int l, int i, int j) { return forward(l + 1, 2 * i, 2 * j); }
+
+   int Encode(int level, int Z, int index[2])
+   {
+      int lmax   = levelMax;
+      int retval = 0;
+
+      int ix = index[0];
+      int iy = index[1];
+      for (int l = level; l >= 0; l--)
+      {
+         int Zp = forward(l, ix, iy);
+         retval += Zp;
+         ix /= 2;
+         iy /= 2;
+      }
+
+      ix = 2 * index[0];
+      iy = 2 * index[1];
+      for (int l = level + 1; l < lmax; l++)
+      {
+         int Zc = forward(l, ix, iy);
+
+         Zc -= Zc % 4;
+         retval += Zc;
+
+         unsigned int ix1, iy1;
+         inverse(Zc, l, ix1, iy1);
+         ix = 2 * ix1;
+         iy = 2 * iy1;
+      }
+
+      retval += level;
+  
+      return retval;
+   }
+};
+
+} // namespace cubism

7bdf8cc6e9be9ab948c09747870fb9ad1bae0bd2
michaich <michaich@ethz.ch>
2020-11-06 11:51:29 +0100

    Much faster SFC constructor

--- include/Cubism/SpaceFillingCurve.h
+++ include/Cubism/SpaceFillingCurve.h
@@ -13,10 +13,14 @@ namespace cubism // AMR_CUBISM
 
 class SpaceFillingCurve
 {
-
- protected:
-  int BX, BY, BZ;
-
+  protected:
+   int BX, BY, BZ;
+   bool isRegular;
+   int base_level;
+   std::vector < std::vector <int> > Zsave;
+   std::vector< std::vector<int> > i_inverse;
+   std::vector< std::vector<int> > j_inverse;
+   std::vector< std::vector<int> > k_inverse;
 
    int AxestoTranspose(const int *X_in, int b) const // position, #bits, dimension
    {
@@ -117,63 +121,20 @@ class SpaceFillingCurve
       } // exchange
    }
 
- public:
-   int *SUBSTRACT;
-   int base_level;
+  public:
    int levelMax;
 
-
    SpaceFillingCurve(){};
-
-   ~SpaceFillingCurve(){ delete[] SUBSTRACT; }
-
-   void __setup(int nx, int ny, int nz)
-   {
-      BX = nx;
-      BY = ny;
-      BZ = nz;
-   }
-
-   std::vector < std::vector <int> > Zsave;
-   std::vector< std::vector<int> > i_inverse;
-   std::vector< std::vector<int> > j_inverse;
-   std::vector< std::vector<int> > k_inverse;
 
 
    SpaceFillingCurve(int a_BX, int a_BY, int a_BZ, int lmax) : BX(a_BX), BY(a_BY), BZ(a_BZ), levelMax(lmax)
    {
-      std::cout << "Constructing Hilbert curve for " << BX << "x" << BY << "x" << BZ << 
-                   " octree with " << lmax << " levels..." << std::endl;
-
-      SUBSTRACT = new int[BX * BY * BZ];
+      std::cout << "Constructing Hilbert curve for " << BX << "x" << BY << "x" << BZ << " octree with " << lmax << " levels..." << std::endl;
 
       int n_max  = max(max(BX, BY), BZ);
       base_level = (log(n_max) / log(2));
       if (base_level < (double)(log(n_max) / log(2))) base_level++;
-      #pragma omp parallel
-      {
-        #pragma omp for collapse(3)
-        for (int k=0;k<BZ;k++)
-        for (int j=0;j<BY;j++)
-        for (int i=0;i<BX;i++)
-        {
-          const int c[3] = {i,j,k};
-          
-          int index = AxestoTranspose( c, base_level);
-    
-          int substract = 0;
-          for (int h=0; h<index; h++)
-          {
-            int X[3] = {0,0,0};
-            TransposetoAxes(h, X, base_level);
-            if (X[0] >= BX ||  
-                X[1] >= BY ||  
-                X[2] >= BZ) substract++;   
-          }   
-          SUBSTRACT[(j + k*BY)*BX + i] = substract;
-        }
-      }
-int count=0;
+
       i_inverse.resize(lmax);
       j_inverse.resize(lmax);
       k_inverse.resize(lmax);
@@ -185,17 +146,31 @@ int count=0;
         j_inverse[l].resize(BX*BY*BZ*aux,-1);
         k_inverse[l].resize(BX*BY*BZ*aux,-1);
         Zsave[l].resize(BX*BY*BZ*aux,-1);
-        for (int k = 0 ; k < BZ* (1<<l); k++)
-        for (int j = 0 ; j < BY* (1<<l); j++)
-        for (int i = 0 ; i < BX* (1<<l); i++)
-        {
-         int n = forward(l,i,j,k);
-         count+=n;
-        }
-
       }
 
-      std::cout << "Hilbert curve ready." <<count<< std::endl;
+      isRegular = true;
+      #pragma omp parallel for collapse(3)
+      for (int k=0;k<BZ;k++)
+      for (int j=0;j<BY;j++)
+      for (int i=0;i<BX;i++)
+      {
+        const int c[3] = {i,j,k};       
+        int index = AxestoTranspose( c, base_level);
+        int substract = 0;
+        for (int h=0; h<index; h++)
+        {
+          int X[3] = {0,0,0};
+          TransposetoAxes(h, X, base_level);
+          if (X[0] >= BX || X[1] >= BY || X[2] >= BZ) substract++;
+        }
+        index -= substract;
+        if (substract > 0) isRegular = false;
+        i_inverse[0][index] = i;
+        j_inverse[0][index] = j;
+        k_inverse[0][index] = k;
+        Zsave[0][k*BX*BY + j*BX + i] = index;
+      }
+      std::cout << "Hilbert curve ready. isRegular=" << isRegular << std::endl;
    }
 
    // space-filling curve (i,j,k) --> 1D index (given level l)
@@ -206,60 +181,89 @@ int count=0;
       if (l >= levelMax) return -1;
       if (Zsave[l][  k*aux*aux*BX*BY + j*aux*BX + i ] != -1) return Zsave[l][  k*aux*aux*BX*BY + j*aux*BX + i ];
 
-      const int I   = i / aux;
-      const int J   = j / aux;
-      const int K   = k / aux;
-      
-      if (I >= BX || J >= BY || K >= BZ) return 0;
-
-      const int c2_a[3] = {i, j, k};
-      int s                      = SUBSTRACT[((J + K * BY) * BX + I)] * aux * aux * aux;
-
-      int retval = AxestoTranspose(c2_a, l + base_level) - s;
-
+      int retval;
+      if (!isRegular)
+      {
+        const int I   = i / aux;
+        const int J   = j / aux;
+        const int K   = k / aux;
+        assert(! (I >= BX || J >= BY || K >= BZ) );
+        const int c2_a[3] = {i-I*aux,j-J*aux,k-K*aux};
+        retval = AxestoTranspose(c2_a, l);
+        retval += IJK_to_index(I,J,K)*aux*aux*aux;
+      }
+      else
+      {
+        const int c2_a[3] = {i,j,k};
+        retval = AxestoTranspose(c2_a, l + base_level);
+      }
       i_inverse[l][retval] = i;
       j_inverse[l][retval] = j;
       k_inverse[l][retval] = k;
       Zsave[l][  k*aux*aux*BX*BY + j*aux*BX + i ] = retval;
-      
       return retval;
    }
 
    void inverse(int Z, int l, int &i, int &j, int &k)
    {
-      assert ( i_inverse[l][Z] != -1);
-      assert ( j_inverse[l][Z] != -1);
-      assert ( k_inverse[l][Z] != -1);
-      i = i_inverse[l][Z];
-      j = j_inverse[l][Z];
-      k = k_inverse[l][Z];
-      return;
-      //int X[3] = {0, 0, 0};
-      //TransposetoAxes(Z, X, l + base_level);
-      //i = X[0];
-      //j = X[1];
-      //k = X[2];
-   }
-
-   void inverse1(int Z, int l, int &i, int &j, int &k)
-   {
-      for (int iz = 0; iz <= 1; iz++)
-      for (int iy = 0; iy <= 1; iy++)
-      for (int ix = 0; ix <= 1; ix++)
+      if (i_inverse[l][Z] != -1)
       {
-        int Ztest = forward(l, ix+i, iy+j, iz+k);
-        if (Ztest == Z)
-        {
-          i = ix+i;
-          j = iy+j;
-          k = iz+k;
-          return;
-        }
-      }      
-      std::cout << "Didn't find inverse in SFC!" << std::endl;
-      abort();
+        assert(j_inverse[l][Z] != -1 && k_inverse[l][Z] != -1);
+        i = i_inverse[l][Z];
+        j = j_inverse[l][Z];
+        k = k_inverse[l][Z];
+        return;
+      }
+      if (isRegular)
+      {
+        int aux   = 1 << l;
+        int X[3] = {0, 0, 0};
+        TransposetoAxes(Z, X, l + base_level);
+        i = X[0];
+        j = X[1];
+        k = X[2];
+        i_inverse[l][Z] = i;
+        j_inverse[l][Z] = j;
+        k_inverse[l][Z] = k;
+        Zsave[l][  k*aux*aux*BX*BY + j*aux*BX + i ] = Z;
+      }
+      else
+      {
+        int aux   = 1 << l;
+        int Zloc  = Z % (aux*aux*aux);
+        int X[3] = {0, 0, 0};
+        TransposetoAxes(Zloc, X, l);
+        int index = Z / (aux*aux*aux);
+        int I,J,K;
+        index_to_IJK(index,I,J,K);
+        i = X[0] + I*aux;
+        j = X[1] + J*aux;
+        k = X[2] + K*aux;
+        i_inverse[l][Z] = i;
+        j_inverse[l][Z] = j;
+        k_inverse[l][Z] = k;
+        Zsave[l][  k*aux*aux*BX*BY + j*aux*BX + i ] = Z;
+      }
+      return;
    }
 
+   int IJK_to_index(int I, int J, int K)
+   {
+     //int index = (J + K * BY) * BX + I;
+     int index = Zsave[0][(J + K * BY) * BX + I];
+     return index;
+   }
+
+   void index_to_IJK(int index, int & I, int & J, int & K)
+   {
+      //K = index / (BX*BY);
+      //J = (index - K*(BX*BY) ) / BX;
+      //I = index - K*(BX*BY) - J*BX;
+      I = i_inverse[0][index];
+      J = j_inverse[0][index];
+      K = k_inverse[0][index];
+      return;
+   }
 
    // return 1D index of CHILD of block (i,j,k) at level l (child is at level l+1)
    int child(int l, int i, int j, int k) { return forward(l + 1, 2 * i, 2 * j, 2 * k); }
@@ -296,7 +300,7 @@ int count=0;
          iy1 = iy;
          iz1 = iz;
 
-         inverse1(Zc, l, ix1, iy1, iz1);
+         inverse(Zc, l, ix1, iy1, iz1);
          ix = 2 * ix1;
          iy = 2 * iy1;
          iz = 2 * iz1;

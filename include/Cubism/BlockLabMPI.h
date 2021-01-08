4334e6efe140d9fad5e48afcbd38718fd3d9cbf1
Michail Chatzimanolakis <mchatzim@daint102.login.cscs.ch>
2021-01-08 11:53:55 +0100

    Clang format applied to Grid* , GrowingVector and BlockLabMPI

--- include/Cubism/BlockLabMPI.h
+++ include/Cubism/BlockLabMPI.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "GridMPI.h"
 #include "AMR_SynchronizerMPI.h"
+#include "GridMPI.h"
 
 CUBISM_NAMESPACE_BEGIN
 
@@ -13,29 +13,23 @@ class BlockLabMPI : public MyBlockLab
 
  private:
    typedef typename MyBlockLab::BlockType BlockType;
-   typedef SynchronizerMPI_AMR<Real,TGrid> SynchronizerMPIType;
+   typedef SynchronizerMPI_AMR<Real, TGrid> SynchronizerMPIType;
    SynchronizerMPIType *refSynchronizerMPI;
 
  public:
-   void prepare(TGrid &grid, int startX, int endX, int startY, int endY,
-                int startZ, int endZ, const bool _istensorial, 
-                int IstartX = 0, int IendX = 0,
-                int IstartY = 0, int IendY = 0, 
-                int IstartZ = 0, int IendZ = 0)
+   void prepare(TGrid &grid, int startX, int endX, int startY, int endY, int startZ, int endZ, const bool _istensorial, int IstartX = 0, int IendX = 0, int IstartY = 0, int IendY = 0, int IstartZ = 0, int IendZ = 0)
    {
-    std::cout << "BlockLabMPI should call prepare with a SynchronizerMPI. You should not be here." << std::endl;
-    MPI_Abort(grid.getWorldComm(),1);
+      std::cout << "BlockLabMPI should call prepare with a SynchronizerMPI. You should not be here." << std::endl;
+      MPI_Abort(grid.getWorldComm(), 1);
    }
 
-   void prepare( TGrid &grid, SynchronizerMPIType &synchronizer)
+   void prepare(TGrid &grid, SynchronizerMPIType &synchronizer)
    {
       refSynchronizerMPI   = &synchronizer;
       StencilInfo stencil  = refSynchronizerMPI->getstencil();
       StencilInfo Cstencil = refSynchronizerMPI->getCstencil();
       assert(stencil.isvalid());
-      MyBlockLab::prepare(grid, stencil.sx, stencil.ex, stencil.sy, stencil.ey, stencil.sz,
-                          stencil.ez, stencil.tensorial, Cstencil.sx, Cstencil.ex, Cstencil.sy,
-                          Cstencil.ey, Cstencil.sz, Cstencil.ez);
+      MyBlockLab::prepare(grid, stencil.sx, stencil.ex, stencil.sy, stencil.ey, stencil.sz, stencil.ez, stencil.tensorial, Cstencil.sx, Cstencil.ex, Cstencil.sy, Cstencil.ey, Cstencil.sz, Cstencil.ez);
    }
 
    void load(const BlockInfo &info, const Real t = 0, const bool applybc = true)
@@ -44,20 +38,14 @@ class BlockLabMPI : public MyBlockLab
 
       typedef typename MyBlockLab::ElementType ET;
       const int gptfloats              = sizeof(ET) / sizeof(Real);
-      const size_t Length[3]           = {MyBlockLab::m_cacheBlock->getSize(0),
-                                MyBlockLab::m_cacheBlock->getSize(1),
-                                MyBlockLab::m_cacheBlock->getSize(2)};
-      const size_t CLength[3]          = {MyBlockLab::m_CoarsenedBlock->getSize(0),
-                                 MyBlockLab::m_CoarsenedBlock->getSize(1),
-                                 MyBlockLab::m_CoarsenedBlock->getSize(2)};
-      const size_t m_nElemsPerSlice[2] = {
-          MyBlockLab::m_cacheBlock->getNumberOfElementsPerSlice(),
-          MyBlockLab::m_CoarsenedBlock->getNumberOfElementsPerSlice()};
-      Real *dst  = &MyBlockLab ::m_cacheBlock    ->LinAccess(0).member(0);
-      Real *dst1 = &MyBlockLab ::m_CoarsenedBlock->LinAccess(0).member(0);
+      const size_t Length[3]           = {MyBlockLab::m_cacheBlock->getSize(0), MyBlockLab::m_cacheBlock->getSize(1), MyBlockLab::m_cacheBlock->getSize(2)};
+      const size_t CLength[3]          = {MyBlockLab::m_CoarsenedBlock->getSize(0), MyBlockLab::m_CoarsenedBlock->getSize(1), MyBlockLab::m_CoarsenedBlock->getSize(2)};
+      const size_t m_nElemsPerSlice[2] = {MyBlockLab::m_cacheBlock->getNumberOfElementsPerSlice(), MyBlockLab::m_CoarsenedBlock->getNumberOfElementsPerSlice()};
+      Real *dst                        = &MyBlockLab ::m_cacheBlock->LinAccess(0).member(0);
+      Real *dst1                       = &MyBlockLab ::m_CoarsenedBlock->LinAccess(0).member(0);
       refSynchronizerMPI->fetch(info, gptfloats, Length, CLength, m_nElemsPerSlice, dst, dst1);
 
-      const std::vector<int> & selcomponents = refSynchronizerMPI->getstencil().selcomponents;
+      const std::vector<int> &selcomponents = refSynchronizerMPI->getstencil().selcomponents;
       MyBlockLab::post_load(info, selcomponents, t, applybc);
    }
 
--- include/Cubism/Grid.h
+++ include/Cubism/Grid.h
@@ -4,7 +4,7 @@
 #include <omp.h>
 
 #ifdef CUBISM_USE_NUMA
-   #include <numa.h>
+#include <numa.h>
 #endif
 
 #include "BlockInfo.h"
@@ -14,21 +14,21 @@ namespace cubism // AMR_CUBISM
 
 struct TreePosition
 {
-    int position{-3};
-    bool CheckCoarser() const {return position == -2;}
-    bool CheckFiner  () const {return position == -1;}
-    bool Exists      () const {return position >=  0;}
-    int  rank        () const {return position;}
-    void setrank(const int r) {position = r;}
-    void setCheckCoarser()    {position = -2;}
-    void setCheckFiner  ()    {position = -1;}
+   int position{-3};
+   bool CheckCoarser() const { return position == -2; }
+   bool CheckFiner() const { return position == -1; }
+   bool Exists() const { return position >= 0; }
+   int rank() const { return position; }
+   void setrank(const int r) { position = r; }
+   void setCheckCoarser() { position = -2; }
+   void setCheckFiner() { position = -1; }
 };
 
 template <typename Block, template <typename X> class allocator = std::allocator>
 class Grid
 {
  protected:
-   std::vector<std::vector<BlockInfo*>> BlockInfoAll;
+   std::vector<std::vector<BlockInfo *>> BlockInfoAll;
    std::vector<std::vector<bool>> ready;
    std::vector<std::vector<TreePosition>> Octree;
    std::vector<BlockInfo> m_vInfo; // meta-data for blocks that belong to this rank
@@ -49,30 +49,28 @@ class Grid
    const bool yperiodic; // grid periodicity in y-direction
    const bool zperiodic; // grid periodicity in z-direction
 
-   TreePosition & Tree(int m, int n) {return Octree[m][n];}
-   TreePosition & Tree(BlockInfo & info) {return Octree[info.level][info.Z];}
-   TreePosition & Tree(const BlockInfo & info) {return Octree[info.level][info.Z];}
-   
+   TreePosition &Tree(int m, int n) { return Octree[m][n]; }
+   TreePosition &Tree(BlockInfo &info) { return Octree[info.level][info.Z]; }
+   TreePosition &Tree(const BlockInfo &info) { return Octree[info.level][info.Z]; }
+
    bool UpdateFluxCorrection{true};
 
    void _alloc() // called in class constructor
    {
       int m        = levelStart;
-      int TwoPower = 1 << m;      
+      int TwoPower = 1 << m;
       for (int n = 0; n < NX * NY * NZ * pow(TwoPower, DIMENSION); n++)
       {
-         Tree(m,n).setrank(0);
+         Tree(m, n).setrank(0);
          _alloc(m, n);
       }
       if (m - 1 >= 0)
       {
-        for (int n = 0; n < NX * NY * NZ * pow( (1<<(m-1)), DIMENSION); n++)
-          Tree(m-1,n).setCheckFiner();
+         for (int n = 0; n < NX * NY * NZ * pow((1 << (m - 1)), DIMENSION); n++) Tree(m - 1, n).setCheckFiner();
       }
       if (m + 1 < levelMax)
       {
-        for (int n = 0; n < NX * NY * NZ * pow( (1<<(m+1)), DIMENSION); n++)
-          Tree(m+1,n).setCheckCoarser();
+         for (int n = 0; n < NX * NY * NZ * pow((1 << (m + 1)), DIMENSION); n++) Tree(m + 1, n).setCheckCoarser();
       }
       FillPos();
    }
@@ -80,27 +78,27 @@ class Grid
    virtual void _alloc(int m, int n) // called whenever the grid is refined
    {
       allocator<Block> alloc;
-      getBlockInfoAll(m,n).ptrBlock = alloc.allocate(1);
-      getBlockInfoAll(m,n).changed  = true;
-      getBlockInfoAll(m,n).h_gridpoint = getBlockInfoAll(m,n).h;
-      m_blocks.push_back((Block *)getBlockInfoAll(m,n).ptrBlock);
+      getBlockInfoAll(m, n).ptrBlock    = alloc.allocate(1);
+      getBlockInfoAll(m, n).changed     = true;
+      getBlockInfoAll(m, n).h_gridpoint = getBlockInfoAll(m, n).h;
+      m_blocks.push_back((Block *)getBlockInfoAll(m, n).ptrBlock);
       m_vInfo.push_back(*BlockInfoAll[m][n]);
    }
 
    void _deallocAll() // called in class destructor
    {
       allocator<Block> alloc;
-      for (size_t i = 0 ; i < m_vInfo.size() ; i ++)
+      for (size_t i = 0; i < m_vInfo.size(); i++)
       {
          int m = m_vInfo[i].level;
          int n = m_vInfo[i].Z;
          alloc.deallocate((Block *)BlockInfoAll[m][n]->ptrBlock, 1);
       }
-      for (int m = 0 ; m < levelMax; m++)
-        for (size_t n = 0 ; n < BlockInfoAll[m].size() ; n++)
-        {
-          if (BlockInfoAll[m][n] != nullptr) delete BlockInfoAll[m][n];
-        }
+      for (int m = 0; m < levelMax; m++)
+         for (size_t n = 0; n < BlockInfoAll[m].size(); n++)
+         {
+            if (BlockInfoAll[m][n] != nullptr) delete BlockInfoAll[m][n];
+         }
       m_blocks.clear();
       m_vInfo.clear();
       BlockInfoAll.clear();
@@ -109,10 +107,8 @@ class Grid
    void _dealloc(int m, int n) // called whenever the grid is compressed
    {
       allocator<Block> alloc;
-      alloc.deallocate((Block *)getBlockInfoAll(m,n).ptrBlock, 1);
-      //getBlockInfoAll(m,n).myrank = -1;
-      //Tree(m,n) = -2;
-      Tree(m,n).setCheckCoarser();
+      alloc.deallocate((Block *)getBlockInfoAll(m, n).ptrBlock, 1);
+      Tree(m, n).setCheckCoarser();
       for (size_t j = 0; j < m_vInfo.size(); j++)
       {
          if (m_vInfo[j].level == m && m_vInfo[j].Z == n)
@@ -130,10 +126,10 @@ class Grid
       {
          if (m == m_vInfo[j].level && n == m_vInfo[j].Z)
          {
-            getBlockInfoAll(m_new,n_new).state   = Leave;
-            getBlockInfoAll(m_new,n_new).changed = true;
-            m_vInfo[j]                         = getBlockInfoAll(m_new,n_new);
-            m_blocks[j]                        = (Block *)getBlockInfoAll(m_new,n_new).ptrBlock;
+            getBlockInfoAll(m_new, n_new).state   = Leave;
+            getBlockInfoAll(m_new, n_new).changed = true;
+            m_vInfo[j]                            = getBlockInfoAll(m_new, n_new);
+            m_blocks[j]                           = (Block *)getBlockInfoAll(m_new, n_new).ptrBlock;
             return;
          }
       }
@@ -146,70 +142,61 @@ class Grid
          {
             int m      = m_vInfo[j].level;
             int n      = m_vInfo[j].Z;
-            m_vInfo[j] = getBlockInfoAll(m,n);
+            m_vInfo[j] = getBlockInfoAll(m, n);
 
-            assert(getBlockInfoAll(m,n).state == m_vInfo[j].state);
-            //assert(getBlockInfoAll(m,n).TreePos == Exists);
-            assert(Tree(m,n).Exists());
+            assert(getBlockInfoAll(m, n).state == m_vInfo[j].state);
+            assert(Tree(m, n).Exists());
 
-            m_blocks[j] = (Block *)getBlockInfoAll(m,n).ptrBlock;
+            m_blocks[j] = (Block *)getBlockInfoAll(m, n).ptrBlock;
          }
       else
          for (size_t j = 0; j < m_vInfo.size(); j++)
          {
             int m            = m_vInfo[j].level;
             int n            = m_vInfo[j].Z;
-            m_vInfo[j].state = getBlockInfoAll(m,n).state;
-            //assert(getBlockInfoAll(m,n).TreePos == Exists);
-            assert(Tree(m,n).Exists());
-            m_blocks[j] = (Block *)getBlockInfoAll(m,n).ptrBlock;
+            m_vInfo[j].state = getBlockInfoAll(m, n).state;
+            assert(Tree(m, n).Exists());
+            m_blocks[j] = (Block *)getBlockInfoAll(m, n).ptrBlock;
          }
       for (size_t j = 0; j < m_vInfo.size(); j++)
       {
-         int m            = m_vInfo[j].level;
-         int n            = m_vInfo[j].Z;
-         m_vInfo[j].blockID = j;
-         getBlockInfoAll(m,n).blockID = j;
+         int m                         = m_vInfo[j].level;
+         int n                         = m_vInfo[j].Z;
+         m_vInfo[j].blockID            = j;
+         getBlockInfoAll(m, n).blockID = j;
       }
    }
 
-   Grid(const unsigned int _NX, const unsigned int _NY = 1, const unsigned int _NZ = 1,
-        const double _maxextent = 1, const unsigned int _levelStart = 0,
-        const unsigned int _levelMax = 1, const bool AllocateBlocks = true,
-        const bool a_xperiodic = true, const bool a_yperiodic = true, const bool a_zperiodic = true)
-       : NX(_NX), NY(_NY), NZ(_NZ), maxextent(_maxextent), levelMax(_levelMax),
-         levelStart(_levelStart), xperiodic(a_xperiodic), yperiodic(a_yperiodic),
-         zperiodic(a_zperiodic)
+   Grid(const unsigned int _NX, const unsigned int _NY = 1, const unsigned int _NZ = 1, const double _maxextent = 1, const unsigned int _levelStart = 0, const unsigned int _levelMax = 1, const bool AllocateBlocks = true, const bool a_xperiodic = true, const bool a_yperiodic = true, const bool a_zperiodic = true) : NX(_NX), NY(_NY), NZ(_NZ), maxextent(_maxextent), levelMax(_levelMax), levelStart(_levelStart), xperiodic(a_xperiodic), yperiodic(a_yperiodic), zperiodic(a_zperiodic)
    {
       BlockInfo dummy;
-     #if DIMENSION == 3
-      int nx     = dummy.blocks_per_dim(0, NX, NY, NZ);
-      int ny     = dummy.blocks_per_dim(1, NX, NY, NZ);
-      int nz     = dummy.blocks_per_dim(2, NX, NY, NZ);
-     #else
-      int nx     = dummy.blocks_per_dim(0, NX, NY);
-      int ny     = dummy.blocks_per_dim(1, NX, NY);
-      int nz     = 1;
-     #endif      
+#if DIMENSION == 3
+      int nx = dummy.blocks_per_dim(0, NX, NY, NZ);
+      int ny = dummy.blocks_per_dim(1, NX, NY, NZ);
+      int nz = dummy.blocks_per_dim(2, NX, NY, NZ);
+#else
+      int nx = dummy.blocks_per_dim(0, NX, NY);
+      int ny = dummy.blocks_per_dim(1, NX, NY);
+      int nz = 1;
+#endif
       int lvlMax = dummy.levelMax(levelMax);
       BlockInfoAll.resize(lvlMax);
       Octree.resize(lvlMax);
       ready.resize(lvlMax);
       for (int m = 0; m < lvlMax; m++)
       {
-        int TwoPower            = 1 << m;
-        const unsigned int Ntot = nx * ny * nz * pow(TwoPower, DIMENSION);
-        BlockInfoAll[m].resize(Ntot,nullptr);
-        ready[m].resize(Ntot,false);
-        //Octree[m].resize(Ntot,-3);
-        Octree[m].resize(Ntot);
+         int TwoPower            = 1 << m;
+         const unsigned int Ntot = nx * ny * nz * pow(TwoPower, DIMENSION);
+         BlockInfoAll[m].resize(Ntot, nullptr);
+         ready[m].resize(Ntot, false);
+         Octree[m].resize(Ntot);
       }
       if (AllocateBlocks) _alloc();
    }
 
    virtual ~Grid() { _deallocAll(); }
 
-   virtual Block *avail(int m, int n){ return (Block *)getBlockInfoAll(m,n).ptrBlock; }
+   virtual Block *avail(int m, int n) { return (Block *)getBlockInfoAll(m, n).ptrBlock; }
 
 #if DIMENSION == 3
    virtual Block *avail1(int ix, int iy, int iz, int m)
@@ -241,7 +228,7 @@ class Grid
    virtual Block &operator()(int ix, int iy, int iz, int m)
    {
       int n = getZforward(m, ix, iy, iz);
-      return *(Block *)getBlockInfoAll(m,n).ptrBlock;
+      return *(Block *)getBlockInfoAll(m, n).ptrBlock;
    }
 #endif
 #if DIMENSION == 2
@@ -256,7 +243,7 @@ class Grid
    virtual Block &operator()(int ix, int iy, int m)
    {
       int n = getZforward(m, ix, iy);
-      return *(Block *)getBlockInfoAll(m,n).ptrBlock;
+      return *(Block *)getBlockInfoAll(m, n).ptrBlock;
    }
 #endif
 
@@ -265,91 +252,87 @@ class Grid
    inline int getlevelMax() { return levelMax; }
    inline int getlevelMax() const { return levelMax; }
 
-   
    BlockInfo &getBlockInfoAll(int m, int n)
    {
-        if (ready[m][n])
-        {
-            return *BlockInfoAll[m][n];
-        }
-        else
-        {
-            #pragma omp critical
+      if (ready[m][n])
+      {
+         return *BlockInfoAll[m][n];
+      }
+      else
+      {
+#pragma omp critical
+         {
+            if (ready[m][n] == false)
             {
-                if (ready[m][n]==false)        
-                {
-                    BlockInfoAll[m][n] = new BlockInfo();                            
-                    int TwoPower = 1 << m;
-                    double h0 = (maxextent / std::max(NX * Block::sizeX, std::max(NY * Block::sizeY, NZ * Block::sizeZ)));
-                    double h = h0 / TwoPower;
-                    double origin[3];
-                    int i,j,k;
-                  #if DIMENSION == 3
-                    BlockInfo::inverse(n,m,i,j,k);           
-                  #else
-                    BlockInfo::inverse(n,m,i,j);
-                    k = 0;           
-                  #endif
-                    origin[0]  = i * Block::sizeX * h;
-                    origin[1]  = j * Block::sizeY * h;
-                    origin[2]  = k * Block::sizeZ * h;
-                    BlockInfoAll[m][n]->setup(m, h, origin, n);
-                    ready[m][n] = true;
-                }
+               BlockInfoAll[m][n] = new BlockInfo();
+               int TwoPower       = 1 << m;
+               double h0          = (maxextent / std::max(NX * Block::sizeX, std::max(NY * Block::sizeY, NZ * Block::sizeZ)));
+               double h           = h0 / TwoPower;
+               double origin[3];
+               int i, j, k;
+#if DIMENSION == 3
+               BlockInfo::inverse(n, m, i, j, k);
+#else
+               BlockInfo::inverse(n, m, i, j);
+               k = 0;
+#endif
+               origin[0] = i * Block::sizeX * h;
+               origin[1] = j * Block::sizeY * h;
+               origin[2] = k * Block::sizeZ * h;
+               BlockInfoAll[m][n]->setup(m, h, origin, n);
+               ready[m][n] = true;
             }
-            return *BlockInfoAll[m][n];
-        }
+         }
+         return *BlockInfoAll[m][n];
+      }
    }
 
-   std::vector<std::vector<BlockInfo*>> &getBlockInfoAll() { return BlockInfoAll; }
+   std::vector<std::vector<BlockInfo *>> &getBlockInfoAll() { return BlockInfoAll; }
 
-   inline        std::vector<Block *> &GetBlocks()       { return m_blocks; }
-   inline const  std::vector<Block *> &GetBlocks() const { return m_blocks; }
+   inline std::vector<Block *> &GetBlocks() { return m_blocks; }
+   inline const std::vector<Block *> &GetBlocks() const { return m_blocks; }
 
-         std::vector<BlockInfo> &getBlocksInfo()       { return m_vInfo; }
+   std::vector<BlockInfo> &getBlocksInfo() { return m_vInfo; }
    const std::vector<BlockInfo> &getBlocksInfo() const { return m_vInfo; }
 
    template <typename T>
-   void apply_permutation_in_place(std::vector<T>& vec, std::vector<std::size_t>& p)
+   void apply_permutation_in_place(std::vector<T> &vec, std::vector<std::size_t> &p)
    {
-       std::vector<bool> done(vec.size(),false);
-       for (std::size_t i = 0; i < vec.size(); ++i)
-       {
-           if (done[i])
-           {
-               continue;
-           }
-           done[i] = true;
-           std::size_t prev_j = i;
-           std::size_t j = p[i];
-           while (i != j)
-           {
-               std::swap(vec[prev_j], vec[j]);
-               done[j] = true;
-               prev_j = j;
-               j = p[j];
-           }
-       }
+      std::vector<bool> done(vec.size(), false);
+      for (std::size_t i = 0; i < vec.size(); ++i)
+      {
+         if (done[i])
+         {
+            continue;
+         }
+         done[i]            = true;
+         std::size_t prev_j = i;
+         std::size_t j      = p[i];
+         while (i != j)
+         {
+            std::swap(vec[prev_j], vec[j]);
+            done[j] = true;
+            prev_j  = j;
+            j       = p[j];
+         }
+      }
    }
    void SortBlocks()
    {
-      for (size_t i = 0 ; i < m_vInfo.size(); i++)
-         m_vInfo[i].blockID = i;
+      for (size_t i = 0; i < m_vInfo.size(); i++) m_vInfo[i].blockID = i;
       std::sort(m_vInfo.begin(), m_vInfo.end());
       std::vector<size_t> permutation(m_vInfo.size());
-      for (size_t i = 0 ; i< m_vInfo.size(); i++)
-         permutation[i] = m_vInfo[i].blockID;
-      apply_permutation_in_place<Block *>(this->m_blocks,permutation);
-      for (size_t i = 0 ; i< m_vInfo.size(); i++)
-         m_vInfo[i].blockID = i;
+      for (size_t i = 0; i < m_vInfo.size(); i++) permutation[i] = m_vInfo[i].blockID;
+      apply_permutation_in_place<Block *>(this->m_blocks, permutation);
+      for (size_t i = 0; i < m_vInfo.size(); i++) m_vInfo[i].blockID = i;
    }
 
    virtual int getBlocksPerDimension(int idim) const
    {
-      std::cout <<"You called Grid::getBlocksPerDimension() in an AMR solver. Do you really need that?"<<std::endl;
+      std::cout << "You called Grid::getBlocksPerDimension() in an AMR solver. Do you really need that?" << std::endl;
       abort();
    }
-   virtual int get_world_size() const {return 1;}
+   virtual int get_world_size() const { return 1; }
 };
 
 } // namespace cubism

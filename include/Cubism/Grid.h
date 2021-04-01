8958555d4f236ba9624aaf451c3050fcfd7be2db
Michail Chatzimanolakis <mchatzim@daint104.login.cscs.ch>
2021-04-01 15:36:52 +0200

    2D BlockGroups

--- include/Cubism/Grid.h
+++ include/Cubism/Grid.h
@@ -476,7 +476,7 @@ class Grid
 
    void UpdateMyGroups()
    {
-      if (!UpdateGroups) return;
+      //if (!UpdateGroups) return; //TODO : does not work for CUP2D
       if (rank() == 0) std::cout << "Updating groups..." << std::endl;
 
       const unsigned int nX = BlockType::sizeX;
@@ -488,6 +488,7 @@ class Grid
       MyGroups.clear();
       std::vector <bool> added(MyInfos.size(),false);
 
+#if DIMENSION == 3
       for (unsigned int m = 0; m < Ngrids; m++)
       {
         const BlockInfo & I = MyInfos[m];
@@ -594,8 +595,108 @@ class Grid
     
             MyGroups.push_back(newGroup);
         }
+      }
+#else
+      for (unsigned int m = 0; m < Ngrids; m++)
+      {
+        const BlockInfo & I = MyInfos[m];
+
+        if (added[I.blockID]) continue;
+        BlockGroup newGroup;
+    
+        newGroup.level = I.level;
+        newGroup.h = I.h;
+        newGroup.Z.push_back(I.Z);
+           
+        std::vector<int > base (3);
+        base[0] = I.index[0];
+        base[1] = I.index[1];
+        base[2] = 0;//I.index[2];
+        std::vector<int > i_off(4,0);
+        std::vector<bool> ready_(4,false);
+
+        int d = 0;
+        auto blk  = getMaxBlocks();
+        do
+        { 
+          if (ready_[d] == false)
+          {
+            bool valid = true;    
+            i_off[d] ++;
+            const int i0 = (d<2) ? (base[d] - i_off[d]) : (base[d-2] + i_off[d]);
+            const int d0 = (d<2) ? (d  )%2 : (d-2    )%2;
+            const int d1 = (d<2) ? (d+1)%2 : (d-2 + 1)%2;
+
+            for (int i1 = base[d1] - i_off[d1]; i1 <= base[d1] + i_off[d1+2]; i1++)
+            {
+              if (valid == false) break;
+
+                if (i0 < 0 || i1 < 0 || i0 >= blk[d0]*(1<<I.level) ||i1 >= blk[d1]*(1<<I.level))
+                {
+                    valid = false;
+                    break;
+                }
+                int n = (d==0||d==2) ? getZforward(I.level,i0,i1) : getZforward(I.level,i1,i0);
+                
+                if (Tree(I.level,n).rank() != rank())
+                {
+                  valid = false;
+                  break;
+                }
+                if (added[getBlockInfoAll(I.level,n).blockID] == true)
+                {
+                    valid = false;
+                }                                   
+            }
+
+             if (valid == false)
+             {
+                 i_off[d] --;
+                 ready_[d] = true;
+             }
+                    else
+                    {
+                        for (int i1 = base[d1] - i_off[d1]; i1 <= base[d1] + i_off[d1+2]; i1++)
+                        {
+                            int n = (d==0||d==2) ? getZforward(I.level,i0,i1) : getZforward(I.level,i1,i0);
+                            newGroup.Z.push_back(n);
+                            added[getBlockInfoAll(I.level,n).blockID] = true;                                   
+                        }
+                    }
+                }
+                d = (d+1)%4;    
+            }while( ready_[0] == false || ready_[1] == false || ready_[2] == false || ready_[3] == false);
+    
+            const int ix_min = base[0] - i_off[0];
+            const int iy_min = base[1] - i_off[1];
+            const int iz_min = 0;//base[2] - i_off[2];      
+            const int ix_max = base[0] + i_off[2];
+            const int iy_max = base[1] + i_off[3];
+            const int iz_max = 0;//base[2] + i_off[5];
+    
+            int n_base = getZforward(I.level,ix_min,iy_min);
+    
+            newGroup.i_min[0] = ix_min;
+            newGroup.i_min[1] = iy_min;
+            newGroup.i_min[2] = iz_min;
+    
+            newGroup.i_max[0] = ix_max;
+            newGroup.i_max[1] = iy_max;
+            newGroup.i_max[2] = iz_max;
+    
+            newGroup.origin[0] =  getBlockInfoAll(I.level,n_base).origin[0];
+            newGroup.origin[1] =  getBlockInfoAll(I.level,n_base).origin[1];
+            newGroup.origin[2] =  getBlockInfoAll(I.level,n_base).origin[2];
+    
+            newGroup.NXX = (newGroup.i_max[0] - newGroup.i_min[0] + 1)*nX + 1;
+            newGroup.NYY = (newGroup.i_max[1] - newGroup.i_min[1] + 1)*nY + 1;
+            newGroup.NZZ = 2;//(newGroup.i_max[2] - newGroup.i_min[2] + 1)*nZ + 1;
+    
+            MyGroups.push_back(newGroup);
+        }
     }
 
+#endif
 };
 
 } // namespace cubism
--- include/Cubism/HDF5Dumper.h
+++ include/Cubism/HDF5Dumper.h
@@ -41,6 +41,169 @@ inline void _warn_no_hdf5(void) {
     fprintf(stderr, "USE OF HDF WAS DISABLED AT COMPILE TIME\n");
 }
 
+template<typename TStreamer, typename hdf5Real, typename TGrid>
+void DumpHDF5_groups(TGrid &grid,
+                     const typename TGrid::Real absTime,
+                     const std::string &fname,
+                     const std::string &dpath = ".",
+                     const bool bXMF = true)
+{
+    typedef typename TGrid::BlockType B;
+    static const int nX = B::sizeX;
+    static const int nY = B::sizeY;
+    static const int nZ = B::sizeZ;
+
+    const int NCHANNELS = TStreamer::NCHANNELS;
+    std::ostringstream filename;
+    std::ostringstream fullpath;
+    filename << fname;// fname is the base filepath without file type extension
+    fullpath << dpath << "/" << filename.str();
+
+    std::vector<BlockGroup> & MyGroups = grid.MyGroups;
+    grid.UpdateMyGroups();
+
+#if DIMENSION==2
+    double hmin = 1e10;
+    for (size_t groupID = 0 ; groupID < MyGroups.size() ; groupID ++) hmin = std::min(hmin,MyGroups[groupID].h);
+#endif
+
+    int start = 0;
+    // Write grid meta-data
+    {
+        std::ostringstream myfilename;
+        myfilename << filename.str();
+        std::stringstream s;
+        s << "<?xml version=\"1.0\" ?>\n";
+        s << "<!DOCTYPE Xdmf SYSTEM \"Xdmf.dtd\" []>\n";
+        s << "<Xdmf Version=\"2.0\">\n";
+        s << "<Domain>\n";
+        s << " <Grid Name=\"OctTree\" GridType=\"Collection\">\n";
+        s << "  <Time Value=\"" << std::scientific << absTime << "\"/>\n\n";
+        for (size_t groupID = 0 ; groupID < MyGroups.size() ; groupID ++)
+        {
+            const BlockGroup & group = MyGroups[groupID];
+            const int nXX = group.NXX;
+            const int nYY = group.NYY;
+            const int nZZ = group.NZZ;
+
+            s << "  <Grid GridType=\"Uniform\">\n";
+            s << "   <Topology TopologyType=\"3DCoRectMesh\" Dimensions=\" "<<nZZ<<" "<<nYY<<" "<<nXX<<"\"/>\n";
+            s << "   <Geometry GeometryType=\"ORIGIN_DXDYDZ\">\n";
+            s << "   <DataItem Dimensions=\"3\" NumberType=\"Double\" Precision=\"8\" " "Format=\"XML\">\n";
+            s << "    " << std::scientific <<group.origin[2]<<" "<<group.origin[1]<<" "<<group.origin[0]<< "\n";
+            s << "   </DataItem>\n";
+            s << "   <DataItem Dimensions=\"3\" NumberType=\"Double\" Precision=\"8\" " "Format=\"XML\">\n";
+#if DIMENSION == 3
+            s << "    " << std::scientific <<group.h<<" "<<group.h <<" "<< group.h << "\n";
+#else
+            s << "    " << std::scientific <<hmin<<" "<<group.h <<" "<< group.h << "\n";
+#endif
+
+            s << "   </DataItem>\n";
+            s << "   </Geometry>\n";
+            int dd = (nZZ - 1)*(nYY - 1)*(nXX - 1);
+            s << "   <Attribute Name=\"data\" AttributeType=\"" << "Scalar"<< "\" Center=\"Cell\">\n";
+            s << "<DataItem ItemType=\"HyperSlab\" Dimensions=\" " << 1 << " " << 1 << " " << dd <<  "\" Type=\"HyperSlab\"> \n";
+            s << "<DataItem Dimensions=\"3 1\" Format=\"XML\">\n";
+            s << start<<"\n";
+            s << 1     <<"\n";
+            s << dd    <<"\n";
+            s << "</DataItem>\n";
+            s << "   <DataItem ItemType=\"Uniform\"  Dimensions=\" " << dd << " " << "\" NumberType=\"Float\" Precision=\" " << (int)sizeof(hdf5Real) << "\" Format=\"HDF\">\n";
+            s << "    " << (myfilename.str() + ".h5").c_str() << ":/" << "dset" << "\n";
+            s << "   </DataItem>\n";
+            s << "   </DataItem>\n";
+            s << "   </Attribute>\n";
+            start += dd;  
+            s << "  </Grid>\n\n";
+        }
+        s << " </Grid>\n";
+        s << "</Domain>\n";
+        s << "</Xdmf>\n";
+        std::string st    = s.str();
+
+        FILE *xmf = 0;
+        xmf = fopen((fullpath.str()+".xmf").c_str(), "w");
+        fprintf(xmf, st.c_str());
+        fclose(xmf);
+    }
+    
+    hid_t file_id, dataset_id, fspace_id, fapl_id;
+    
+    H5open();
+
+    //1.Set up file access property list
+    fapl_id = H5Pcreate(H5P_FILE_ACCESS);
+
+    //2.Create a new file and release property list identifier.
+    file_id = H5Fcreate((fullpath.str()+".h5").c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, fapl_id);        
+    H5Pclose(fapl_id);
+
+    //3.Create dataset
+    fapl_id = H5Pcreate(H5P_DATASET_XFER);
+    int total = start;
+    hsize_t dims[1]  = {(hsize_t) total};
+    fspace_id        = H5Screate_simple(1, dims, NULL);
+    dataset_id       = H5Dcreate (file_id, "dset", get_hdf5_type<hdf5Real>(), fspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+    //4.Dump
+    int start1 = 0;
+    std::vector<hdf5Real> bigArray(start);
+    for (size_t groupID = 0 ; groupID < MyGroups.size() ; groupID ++)
+    {
+        const BlockGroup & group = MyGroups[groupID];
+        const int nX_max = group.NXX-1;
+        const int nY_max = group.NYY-1;
+        const int nZ_max = group.NZZ-1;
+        int dd1 = nX_max * nY_max * nZ_max;// * NCHANNELS;
+        std::vector<hdf5Real> array_block( dd1, 0.0);
+        for (int kB = group.i_min[2]; kB <= group.i_max[2]; kB++)
+        for (int jB = group.i_min[1]; jB <= group.i_max[1]; jB++)
+        for (int iB = group.i_min[0]; iB <= group.i_max[0]; iB++)
+        {
+            int Z = BlockInfo::forward(group.level,iB,jB);//,kB);
+            const cubism::BlockInfo& I = grid.getBlockInfoAll(group.level,Z);
+            //lab.load(I, 0);
+            B & block = * (B*)I.ptrBlock;
+
+            for (int iz = 0; iz < nZ; iz++)
+            for (int iy = 0; iy < nY; iy++)
+            for (int ix = 0; ix < nX; ix++)
+            {
+                hdf5Real output[NCHANNELS];
+                TStreamer::operate(block,ix,iy,iz,output);
+                const int iz_b = (kB-group.i_min[2])*nZ + iz;
+                const int iy_b = (jB-group.i_min[1])*nY + iy;
+                const int ix_b = (iB-group.i_min[0])*nX + ix;
+                const int base = iz_b *(nX_max*nY_max) + iy_b *nX_max + ix_b;               
+                if (NCHANNELS > 1)
+                {
+                  output[0] = output[0]*output[0] + output[1]*output[1] + output[2]*output[2];
+                  array_block[base] = sqrt(output[0]);
+                }
+                else
+                {
+                  array_block[base] = output[0];
+                }
+            }
+        }
+        for (int j = 0 ; j < dd1 ; j ++)
+        {
+            bigArray      [start1 + j] = array_block[j];
+        }
+        start1 += dd1;
+    }
+
+    dataset_id = H5Dopen(file_id, "dset", H5P_DEFAULT);
+    H5Dwrite(dataset_id, get_hdf5_type<hdf5Real>(), H5S_ALL, H5S_ALL, H5P_DEFAULT, bigArray.data());
+    H5Dclose(dataset_id);
+
+    H5Sclose(fspace_id);
+    H5Dclose(dataset_id);
+    H5Pclose(fapl_id);
+    H5Fclose(file_id);
+    H5close();
+}
 
 // The following requirements for the data TStreamer are required:
 // TStreamer::NCHANNELS        : Number of data elements (1=Scalar, 3=Vector, 9=Tensor)

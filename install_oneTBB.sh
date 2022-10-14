e2a2e40946f3eb973d4b7b1d305274a9c4f30011
Michail Chatzimanolakis <michaich@ethz.ch>
2022-10-14 16:43:20 +0300

    typo fix

--- install_oneTBB.sh
+++ install_oneTBB.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/oneapi-src/oneTBB.git
 cd oneTBB
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=installed_onetbb -DTBB_TEST=OFF ..
-cmake --build .
-cmake --install .
+cmake -DCMAKE_INSTALL_PREFIX=../../my_installed_onetbb -DTBB_TEST=OFF ..
+cmake --build . --config relwithdebinfo
+cmake --install . --config relwithdebinfo

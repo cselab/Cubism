6ad80667bf07cd30a29db20418d40d2662119b7f
Michail Chatzimanolakis <mchatzim@daint104.login.cscs.ch>
2023-05-04 09:07:39 +0200

    Put templates in 'ConsistentOperations.h' to avoid defining the same functions multiple times

--- include/Cubism/ConsistentOperations.h
+++ include/Cubism/ConsistentOperations.h
@@ -2,32 +2,28 @@
 
 //Functions suggested in "Numerical symmetry-preserving techniques for low-dissipation shock-capturing schemes" equation (19) for summation that does not favor a spatial direction
 
-namespace cubism {
+namespace cubism
+{
 #ifdef PRESERVE_SYMMETRY
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-function" //silence annoying warnings
-static Real ConsistentSum(const Real a, const Real b, const Real c)
+
+template <typename T>
+static T ConsistentSum(const T a, const T b, const T c)
 {
-   const Real s1 = a + (b+c);
-   const Real s2 = b + (c+a);
-   const Real s3 = c + (a+b);
-   return 0.5*(std::min({s1,s2,s3})+std::max({s1,s2,s3}));
-}
-static Real ConsistentSum(const Real a, const Real b, const Real c, const Real d)
-{
-   const Real s1 = (a+b)+(c+d);
-   const Real s2 = (c+a)+(b+d);
-   const Real s3 = (b+c)+(a+d);
+   const T s1 = a + (b+c);
+   const T s2 = b + (c+a);
+   const T s3 = c + (a+b);
    return 0.5*(std::min({s1,s2,s3})+std::max({s1,s2,s3}));
 }
 
-static Real ConsistentAverage(const Real e000, const Real e001, const Real e010, const Real e011, const Real e100, const Real e101, const Real e110, const Real e111)
+template <typename T>
+static T ConsistentSum(const T a, const T b, const T c, const T d)
 {
-   const Real a = e000 + e111;
-   const Real b = e001 + e110;
-   const Real c = e010 + e101;
-   const Real d = e100 + e011;
-   return 0.125*ConsistentSum(a,b,c,d);
+   const T s1 = (a+b)+(c+d);
+   const T s2 = (c+a)+(b+d);
+   const T s3 = (b+c)+(a+d);
+   return 0.5*(std::min({s1,s2,s3})+std::max({s1,s2,s3}));
 }
 
 template <typename T>
@@ -40,27 +36,6 @@ static T ConsistentAverage(const T e000, const T e001, const T e010, const T e01
    return 0.125*ConsistentSum(a,b,c,d);
 }
 
-template <typename T>
-static T ConsistentSum(const T a, const T b, const T c)
-{
-   T s1 = a + (b+c);
-   T s2 = b + (c+a);
-   T s3 = c + (a+b);
-   T result;
-   for (int i = 0 ; i < T::DIM ; i ++) result.member(i) = 0.5*(std::min({s1.member(i),s2.member(i),s3.member(i)})+std::max({s1.member(i),s2.member(i),s3.member(i)}));
-   return result;
-}
-
-template <typename T>
-static T ConsistentSum(const T a, const T b, const T c, const T d)
-{
-   T s1 = (a+b)+(c+d);
-   T s2 = (c+a)+(b+d);
-   T s3 = (b+c)+(a+d);
-   T result;
-   for (int i = 0 ; i < T::DIM ; i ++) result.member(i) = 0.5*(std::min({s1.member(i),s2.member(i),s3.member(i)})+std::max({s1.member(i),s2.member(i),s3.member(i)}));
-   return result;
-}
 #pragma GCC diagnostic pop
 #endif
 }
